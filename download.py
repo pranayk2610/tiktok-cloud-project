b17f26e5a4457daf448366c1e804a687b20ba0af
pranayk2610 <pranaykumar2610@gmail.com>
2021-11-07 23:01:38 -0500

    basic search and download from tiktok

--- download.py
+++ download.py
@@ -1,5 +1,25 @@
-import youtube_dl
+from TikTokApi import TikTokApi
+import pprint
+import sys
 
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([''])
+api = TikTokApi.get_instance()
+
+device_id = api.generate_device_id()
+
+if (len(sys.argv) > 1):
+    count = sys.argv[1]
+    search = sys.argv[2]
+else:
+    count = 3
+    search = 'triggerwarning'
+
+tiktoks = api.by_hashtag(hashtag=search, count=count)
+
+video_bytes = api.get_video_by_tiktok(tiktoks[0], custom_device_id=device_id)
+
+pprint.pp(tiktoks[0])
+
+with open("video.mp4", "wb") as out:
+    out.write(video_bytes)
+
+
