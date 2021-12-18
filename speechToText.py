2c374c5fa7ee7a27da837ed56b8a82530c792cc7
bristolking1 <71149056+bristolking1@users.noreply.github.com>
2021-12-17 22:38:27 -0500

    Update speechToText.py

--- speechToText.py
+++ speechToText.py
@@ -56,16 +56,6 @@ print('\n')
 
 # # recognize speech using Google Cloud Speech
 GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""{
-  "type": "service_account",
-  "project_id": "central-hangar-333703",
-  "private_key_id": "e8f2e45a260fce1015c2a73b251339d18aa1532b",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC1fbDXn87hJ9mQ\ntQrHSj9Y9HNo6vcRjNm/jOtUoaoDTPSBC43sRIb1mGYqkQ3Ts3k5CPIAOwWJ+Ybh\nQylPcR3JCM/K+oLnL9yAZ8vbIbAk/9ql6pe/NiwQjx1T+/lBR/9+nmT2GcymgJaj\nsFa8IAI1/9V5+nwn7q4ejAHISHd3lmy6Dj7GzTo5Kn1nSdhMAE4A5AE1SGLHhGqI\nngs/DSbDcDjFl8tCDa+6A4YBsTCrl8nHcoe2REqPS53ZPwdhUf2/L4ZLmh4j90/l\nCjMnMCnGpBMIfLspb64P20UrYC9nf/gG8BVvZ3Gg4Ac1t9eccEDo2Gp0xIjesYgX\nvOH92XgVAgMBAAECggEAIYtesSvfX+wuoTllCZqmhLmBz0WGbt76rBXOtTDALKX/\nlw5cp+uuihwl3f47cQJ+CFWn7PT7vBm7nUz9iKpel/ah+WE96p8FAVKt8g2S8fO8\nJOhsLbJEiLb687fhxPgat/tsq7PvCGNB9HU2QsUjw3lKqqXqZrMNyaWU+sCyHA8I\nKRcEFldbr+TohfP/Lf3vUSV9nePcSSm9uB8n5MSR9Hjs5jgtBejdIhTOEF5MXSKY\nqUOFdVMZ7UQXI4Inx0O/6IE1FSHMWhKWBWsPh8xQfNSeR2P+gGSWuhRGgia/7Lv1\nOOWzyDaC5YBjAhhFMvJOPhdrsvCbz2I1PcQAfGmNTQKBgQD1FH8eAStiX5Ew1YI8\nKxnn19LHp/2eGj0whKqZ/2ZKRDpPnOtHyxGI++SZbeaeP/BK6nk8EVWrwbL5xTLz\ngB/gcEaJkwOw6Lqw5BQXvOnbA0uAG8ln4krQnFmPYC0D/HtgS3IVJHZ8VWNFitRO\nSuEYH0NcN1lkITisrMRt4kajZwKBgQC9k92uN2jy8FkQJjzBLV3oOCPeywlVK/Tu\nnHAMzICSYMIxSYlBoSvkkoA5NOlkNjTYn6NehT1sxToJAM8JInKHOmLgpcJrCUFl\nPlrVlx/8MC98iw3d8//rcvpwJauiNC1G9chAtkFO/Rgap2q59xf6mb3Dxq3u6UMU\nBCAzrno3IwKBgD4iByQtObmD7wRJbO4iIzwy7D4v6c12a4Lsb3ABXwZ/C6V5a/DU\nm8p1yfvX7/q4zQvBLCE9Z9HWlrBZ/VPcbLhy3n8sZWgpaINGH43xF/ytdiuxX7tV\nlJAUw2PlBySWkxTTq8WOa/eTkkpkKvT6aNy7uFEduyz2ey7T0izli/ELAoGAF0NP\nzpMKE9WeeDbY+bOBdH9/F1W+3W65bZZQwaFOnae17KpjZ2RJqJC5n0t44E0uiu57\n85tSAEjraDNrNGm/uAxYGcz9YHkQR0Yxi0gyKpYXHadKmlPYsaG4TY6x0wOoEq8S\nVa6HsROpo4urswo5LQ00lhOJQGnrS0d+VBky8hUCgYEA8sBTVqNp55YV4pDu5R9f\n9m8wzi5iGrtZh2PcqjdEWWqW4d1nu/rSHqTCwgVM53x9tUOXMW5UwZdk8Yjp1NQS\nhosNjQldPIYL6S9oMpt1CUTIOLrGvNLK7uTSjtid/dpTbj3UKCl7OReyBvCn6hUZ\n5bzllUK/vROQunrSs+44DDg=\n-----END PRIVATE KEY-----\n",
-  "client_email": "tiktokcloud@central-hangar-333703.iam.gserviceaccount.com",
-  "client_id": "110193573904094277009",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/tiktokcloud%40central-hangar-333703.iam.gserviceaccount.com"
 }"""
 try:
     with open('Transcripts/Video30/VideoTransciptGCloudSpeech30.txt', 'w') as f:
@@ -110,8 +100,8 @@ print('\n')
 #     print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
 
 # # recognize speech using Houndify
-HOUNDIFY_CLIENT_ID = "nt8BIyFxK4j-pBpLd4y9RQ=="  # Houndify client IDs are Base64-encoded strings
-HOUNDIFY_CLIENT_KEY = "Nd9sfmOzaOTys7-bqcV-XD0N73h0hc2ZrC4UfEqBVMF4jaiOEQce7MkWvzkN84x1-0DdkMEwLeQecuVEST0Nww=="  # Houndify client keys are Base64-encoded strings
+HOUNDIFY_CLIENT_ID = ""  # Houndify client IDs are Base64-encoded strings
+HOUNDIFY_CLIENT_KEY = ""  # Houndify client keys are Base64-encoded strings
 try:
      with open('Transcripts/Video30/VideoTranscriptHoundify30.txt', 'w') as f:
         sys.stdout = f 
@@ -126,8 +116,8 @@ print(r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUND
 print('\n')
 
 # # recognize speech using IBM Speech to Text
-apikey = "q2KvlsrAPFJuEmmso8DUxM400leEmbIfUYnlGeJbpJWP"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-url = "https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/1c3bc4c1-0bb3-4a3f-9bba-804e091749c5"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
+apikey = ""  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+url = ""  # IBM Speech to Text passwords are mixed-case alphanumeric strings
 
 authenticator = IAMAuthenticator(apikey)
 stt = SpeechToTextV1(authenticator = authenticator)
